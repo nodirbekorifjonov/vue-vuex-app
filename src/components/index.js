8ee7f8526f8ac18857b95470575cbff2917c3a03
Nodirbek Orifjonov <nodirbekorifjonov@mail.ru>
2025-02-06 19:05:16 +0500

    register, login, components, vuex, modules

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Navbar } from './navbar/Navbar.vue'
+export { default as Register } from './register/Register.vue'
+export { default as Login } from './login/Login.vue'
---
+++ src/ui-components/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="form-floating">
+    <input
+      :type="type"
+      class="form-control"
+      id="floatingInput"
+      :placeholder="label"
+    />
+    <label for="floatingInput">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    label: String,
+    type: String,
+  },
+};
+</script>
+
+<style></style>
